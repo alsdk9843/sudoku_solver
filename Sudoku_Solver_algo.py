e5b2af41dd55e63548f23ffd8a72d9e93fc600bd
alsdk9843 <44119261+alsdk9843@users.noreply.github.com>
2021-12-10 23:17:33 -0800

    Add files via upload
    Creating an algorithm to fill the "zeros positions in the matrix" with "Sudoku answers"

---
+++ Sudoku_Solver_algo.py
@@ -0,0 +1,9 @@
+
+import Sudoku_Solver_matrix_gen
+
+# Import the 'Generated data matrix' from the 'Sudoku_Solver_matrix_gen.py' file
+
+Sudoku_gen_data_matrix = Sudoku_Solver_matrix_gen.gen_matrix
+
+
+
